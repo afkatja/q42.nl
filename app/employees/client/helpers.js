01d42115e96a295c1070943b5b540a1b1e300cb1
Rahul Choudhury <rchoudhury@gmail.com>
2016-08-19 11:31:55 +0200

    Use robohash instead of stormtrooper for fallback images

--- app/employees/client/helpers.js
+++ app/employees/client/helpers.js
@@ -1,9 +1,18 @@
 import { Template } from 'meteor/templating'
+import { Utils } from '../../../lib/utils'
 
 Template.registerHelper("avatar_static", function() {
-  return this.imageStatic || `${this.handle}.jpg`;
+  const assetsUrl = Utils.getStaticAssetsUrl();
+  if (this.imageStatic)
+    return `https://robohash.org/${this.handle}@q42.nl?size=108x108`;
+
+  return `${assetsUrl}/images/employees/${this.handle}.jpg`;
 });
 
 Template.registerHelper("avatar_animated", function() {
-  return this.imageAnimated || `${this.handle}.gif`;
+  const assetsUrl = Utils.getStaticAssetsUrl();
+  if (this.imageAnimated)
+    return `https://robohash.org/${this.handle}@q42.nl?size=108x108`;
+
+  return `${assetsUrl}/images/employees/${this.handle}.gif`;
 });
