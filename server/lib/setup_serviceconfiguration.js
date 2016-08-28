b02fa15c2d5432e3b3e52ecfcedd97ed2f11ee6b
Rahul Choudhury <rchoudhury@gmail.com>
2016-08-29 00:57:04 +0200

    Use new object shorthand

--- server/lib/setup_serviceconfiguration.js
+++ server/lib/setup_serviceconfiguration.js
@@ -3,10 +3,9 @@ import { ServiceConfiguration } from 'meteor/service-configuration'
 
 Meteor.startup(() => {
   _.each(Meteor.settings.private.serviceConfiguration, (settings) => {
-    settings = _.extend(settings, {loginStyle: "popup"});
     ServiceConfiguration.configurations.upsert(
-      {service: settings.service},
-      settings
+      { service: settings.service },
+      { settings, loginStyle: "popup" }
     );
   });
 });
