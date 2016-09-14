bdc9e0c912c235c9453171e3f924f3f732efe83d
Rahul Choudhury <rchoudhury@gmail.com>
2016-09-14 15:12:33 +0200

    Remove Meteor accounts system, since nothing depends on it if we remove chat

--- server/lib/fastrender.js
+++ server/lib/fastrender.js
@@ -2,7 +2,6 @@ import { FastRender } from 'meteor/meteorhacks:fast-render'
 
 FastRender.onAllRoutes(function() {
   this.subscribe("employeeCount");
-  this.subscribe("allUserData");
 });
 
 FastRender.route( "/", function() {
