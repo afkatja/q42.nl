ced37f3b58c834fab4f91f944473147952ff7e4d
Rahul Choudhury <rchoudhury@gmail.com>
2016-09-14 10:29:44 +0200

    Set layout root to body

--- client/startup.js
+++ client/startup.js
@@ -4,6 +4,8 @@ import { $ } from 'meteor/jquery'
 Meteor.startup( () => {
   $.ajaxSetup({cache: true});
 
+  BlazeLayout.setRoot('body');
+
   // see 6_animation.styl#4
   const isFirefox = /Firefox/.test(navigator.userAgent);
   const isIE11 = !window.ActiveXObject && "ActiveXObject" in window;
